58cf98d44edf7a328f435a980f382e719cb549f7
hirendra <hirendrathaku@blackbuck.com>
2016-11-18 15:21:16 +0530

    added icons

--- cricketscore/cricketscore.py
+++ cricketscore/cricketscore.py
@@ -3,6 +3,7 @@ import urllib
 from bs4 import BeautifulSoup
 import subprocess
 import time
+import os
 
 class CricketScore():
 	def __init__(self):
@@ -18,12 +19,27 @@ class CricketScore():
 
 	def start_process(self):
 		previous_score = ''
+		previous_runs = 0
+		previous_wickets = 0
 		while(True):
 			current_score = self.get_score()
+			split_list = current_score.split('/')
+			current_runs = split_list[0]
+			current_wickets = split_list[1].split(" ")[0]
+			print current_score
 			if previous_score!=current_score:
 				previous_score = current_score
-				print current_score
-				subprocess.Popen(['notify-send','Cricket Score',current_score])
+				base_dir = os.path.dirname(os.path.abspath(__file__))
+				self.image_path = base_dir+"/cricket.png"
+
+				if previous_runs!=current_runs:
+					previous_runs = current_runs
+					self.image_path = base_dir+"/bat.jpg"
+				if previous_wickets!=current_wickets:
+					previous_wickets = current_wickets
+					self.image_path = base_dir+"/out.png"
+
+				subprocess.Popen(['notify-send','-i',self.image_path,'Cricket Score',current_score])
 				time.sleep(5)
 
 if __name__ == '__main__':
